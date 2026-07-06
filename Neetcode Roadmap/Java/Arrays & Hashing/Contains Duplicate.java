fb564d919786711392188168e16fb3781af00e7c
Joshua S. <johnsilverknight@gmail.com>
2026-07-06 13:21:19 -0500

    Separated into different folders for different language solutions

---
+++ Neetcode Roadmap/Java/Arrays & Hashing/Contains Duplicate.java
@@ -0,0 +1,20 @@
+/*
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+*/
+import java.util.HashMap;
+
+class Solution {
+
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> seen = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (!seen.containsKey(nums[i])) {
+        seen.put(nums[i], 0);
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
